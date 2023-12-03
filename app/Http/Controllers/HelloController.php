6b85eb33c6d664c2ae57572fbdab3bfd63fb2da1
onigra <3280467rec@gmail.com>
2023-12-03 15:20:02 +0900

    適当になんか返す

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class HelloController extends Controller
 {
-    //
+    public function index(): JsonResponse
+    {
+        return response()->json(['foo' => 'bar']);
+    }
 }
