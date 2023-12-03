10832b7b08f8544780e99b53b7f9e7d6c5b6188c
onigra <3280467rec@gmail.com>
2023-12-03 22:54:46 +0900

    引数ありのテストと分けた

--- tests/Unit/HelloTest.php
+++ tests/Unit/HelloTest.php
@@ -14,14 +14,24 @@ class HelloTest extends TestCase
      */
     public function test_say_hello()
     {
+        // when
+        $actual = Hello::say_hello();
+
+        // then
+        $expected = collect(['Hello' => 'world']);
+        $this->assertEquals($actual, $expected);
+    }
+
+    public function test_say_hello_with_name()
+    {
         // given
-        $name = 'world';
+        $name = 'Sae-byeok';
 
         // when
         $actual = Hello::say_hello($name);
 
         // then
-        $expected = collect(['Hello' => 'world']);
+        $expected = collect(['Hello' => 'Sae-byeok']);
         $this->assertEquals($actual, $expected);
     }
 }
